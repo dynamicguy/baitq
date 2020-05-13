35e2b81de64f4255c0159b81a02fff8f796408a3
Nurul Ferdous <nurul@ferdo.us>
2020-05-13 22:10:21 +0600

    fix: blogs img changed

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -11,10 +11,7 @@
       <div v-for="(post, index) in posts" :key="index" class="w-full md:w-1/2 my-4 md:px-4">
         <div class="post">
           <nuxt-link :to="`/blog/${post.slug}`">
-            <img
-              class="w-full"
-              :src="post.featuredImage || 'https://source.unsplash.com/random/640x340'"
-            />
+            <img class="w-full" :src="post.featuredImage || '/images/uploads/logo.png'" />
             <div class="p-6 bg-white">
               <h2 class="text-2xl mb-2">{{ post.title }}</h2>
 
